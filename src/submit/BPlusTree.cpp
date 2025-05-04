2ba9ec0c174f2f1c01cabbbf868e7b22292954a4
TheUnknownThing <wanghn051@outlook.com>
2025-05-04 21:47:50 +0800

    refactor: Replace std::hash with CustomStringHasher for index handling in BPlusTree

--- src/submit/BPlusTree.cpp
+++ src/submit/BPlusTree.cpp
@@ -1,7 +1,22 @@
 #include "src/include/storage/bptStorage.hpp"
 #include "utils/string64.hpp"
 #include <iostream>
-#include <functional>
+#include <cstddef>
+#include <cstring>
+
+struct CustomStringHasher {
+    size_t operator()(const char* str) const {
+        size_t hash = 5381;
+        const size_t salt = 33;
+        int c;
+
+        while ((c = *str++)) {
+            hash = ((hash << 5) + hash) + c + salt;
+        }
+
+        return hash;
+    }
+};
 
 signed main() {
   std::ios::sync_with_stdio(false);
@@ -11,32 +26,30 @@ signed main() {
   std::cin >> n;
   std::string op;
 
-  size_t MAXN = ULONG_MAX;
+  BPTStorage<size_t, int, 55, 53> book("data", ULONG_MAX);
 
-  BPTStorage<size_t, int, 55, 53> book("data", MAXN);
-
-  std::hash<std::string> string_hasher;
+  CustomStringHasher custom_hasher;
 
   for (int i = 0; i < n; ++i) {
     std::cin >> op;
     if (op == "insert") {
-      std::string index_str;
+      sjtu::string64 index_str;
       std::cin >> index_str;
       int value;
       std::cin >> value;
-      size_t index_hash = string_hasher(index_str); 
+      size_t index_hash = custom_hasher(index_str.c_str());
       book.insert(index_hash, value);
     } else if (op == "delete") {
-      std::string index_str;
+      sjtu::string64 index_str;
       std::cin >> index_str;
       int value;
       std::cin >> value;
-      size_t index_hash = string_hasher(index_str);
+      size_t index_hash = custom_hasher(index_str.c_str());
       book.remove(index_hash, value);
     } else if (op == "find") {
-      std::string index_str;
+      sjtu::string64 index_str;
       std::cin >> index_str;
-      size_t index_hash = string_hasher(index_str);
+      size_t index_hash = custom_hasher(index_str.c_str());
       auto result = book.find(index_hash);
       for (const auto &val : result) {
         std::cout << val << " ";
