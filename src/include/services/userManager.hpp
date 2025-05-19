d69d8e8a38444a4a295ef8ac71dfbdc104bc2b90
TheUnknownThing <wanghn051@outlook.com>
2025-05-19 17:55:00 +0800

    refactor: Reorganize includes and improve formatting in UserManager and TicketSystem

--- src/include/services/userManager.hpp
+++ src/include/services/userManager.hpp
@@ -1,8 +1,8 @@
 #ifndef USER_MANAGER_HPP
 #define USER_MANAGER_HPP
 
-#include "storage/bptStorage.hpp"
 #include "stl/map.hpp"
+#include "storage/bptStorage.hpp"
 #include "utils/string32.hpp"
 #include <string>
 
@@ -10,137 +10,158 @@ using sjtu::map;
 using sjtu::string32;
 
 struct User {
-    string32 username;
-    string32 password;
-    string32 name;
-    string32 mailAddr;
-    int privilege;
-    
-    User() = default;
-    User(const string32& un, const string32& pw, const string32& nm, 
-         const string32& ma, int priv)
-        : username(un), password(pw), name(nm), mailAddr(ma), privilege(priv) {}
+  string32 username;
+  string32 password;
+  string32 name;
+  string32 mailAddr;
+  int privilege;
 
-    bool operator==(const User& other) const {
-        return username == other.username && password == other.password &&
-               name == other.name && mailAddr == other.mailAddr &&
-               privilege == other.privilege;
-    }
-    bool operator!=(const User& other) const {
-        return !(*this == other);
-    }
+  User() = default;
+  User(const string32 &un, const string32 &pw, const string32 &nm,
+       const string32 &ma, int priv)
+      : username(un), password(pw), name(nm), mailAddr(ma), privilege(priv) {}
+
+  bool operator==(const User &other) const {
+    return username == other.username && password == other.password &&
+           name == other.name && mailAddr == other.mailAddr &&
+           privilege == other.privilege;
+  }
+  bool operator!=(const User &other) const { return !(*this == other); }
+
+  friend std::ostream &operator<<(std::ostream &os, const User &user) {
+    if (user == User())
+      return os << "-1";
+    os << user.username << " " << user.name << " " << user.mailAddr
+       << " " << user.privilege;
+    return os;
+  }
 };
 
 class UserManager {
 private:
-    BPTStorage<string32, User> userDB;
-    map<string32, int> loggedInUsers; // username -> privilege
-    
+  BPTStorage<string32, User> userDB;
+  map<string32, int> loggedInUsers; // username -> privilege
+
 public:
-    UserManager(const std::string& filename);
-    
-    bool addUser(const string32& curUser, const string32& username, 
-                const string32& password, const string32& name,
-                const string32& mailAddr, int privilege);
-    
-    bool login(const string32& username, const string32& password);
-    bool logout(const string32& username);
-    
-    User queryProfile(const string32& curUser, const string32& username);
-    bool modifyProfile(const string32& curUser, const string32& username,
-                      const string32& password, const string32& name,
-                      const string32& mailAddr, int privilege);
-    
-    bool isLoggedIn(const string32& username) const;
-    int getPrivilege(const string32& username) const;
-    
-    bool isFirstUser() const;
+  UserManager(const std::string &filename);
+
+  bool addUser(const string32 &curUser, const string32 &username,
+               const string32 &password, const string32 &name,
+               const string32 &mailAddr, int privilege);
+
+  bool login(const string32 &username, const string32 &password);
+  bool logout(const string32 &username);
+
+  User queryProfile(const string32 &curUser, const string32 &username);
+  User modifyProfile(const string32 &curUser, const string32 &username,
+                     const string32 &password, const string32 &name,
+                     const string32 &mailAddr, int privilege);
+
+  bool isLoggedIn(const string32 &username) const;
+  int getPrivilege(const string32 &username) const;
+
+  bool isFirstUser() const;
 };
 
-UserManager::UserManager(const std::string& filename) 
+UserManager::UserManager(const std::string &filename)
     : userDB(filename + "_user", string32::string32_MAX()) {}
 
-bool UserManager::addUser(const string32& curUser, const string32& username, 
-                        const string32& password, const string32& name,
-                        const string32& mailAddr, int privilege) {
-    if (userDB.find(username).size() > 0) return false;
-    
-    if (!isFirstUser()) {
-        if (!isLoggedIn(curUser)) return false;
-        if (getPrivilege(curUser) <= privilege) return false;
-    } else {
-        privilege = 10; // First user gets max privilege
-    }
-    
-    User newUser(username, password, name, mailAddr, privilege);
-    userDB.insert(username, newUser);
-    return true;
+bool UserManager::addUser(const string32 &curUser, const string32 &username,
+                          const string32 &password, const string32 &name,
+                          const string32 &mailAddr, int privilege) {
+  if (userDB.find(username).size() > 0)
+    return false;
+
+  if (!isFirstUser()) {
+    if (!isLoggedIn(curUser))
+      return false;
+    if (getPrivilege(curUser) <= privilege)
+      return false;
+  } else {
+    privilege = 10; // First user gets max privilege
+  }
+
+  User newUser(username, password, name, mailAddr, privilege);
+  userDB.insert(username, newUser);
+  return true;
 }
 
-bool UserManager::login(const string32& username, const string32& password) {
-    auto users = userDB.find(username);
-    if (users.empty() || users[0].password != password) return false;
-    if (isLoggedIn(username)) return false;
-    
-    loggedInUsers[username] = users[0].privilege;
-    return true;
+bool UserManager::login(const string32 &username, const string32 &password) {
+  auto users = userDB.find(username);
+  if (users.empty() || users[0].password != password)
+    return false;
+  if (isLoggedIn(username))
+    return false;
+
+  loggedInUsers[username] = users[0].privilege;
+  return true;
 }
 
-bool UserManager::logout(const string32& username) {
-    if (!isLoggedIn(username)) return false;
-    loggedInUsers.erase(loggedInUsers.find(username));
-    return true;
+bool UserManager::logout(const string32 &username) {
+  if (!isLoggedIn(username))
+    return false;
+  loggedInUsers.erase(loggedInUsers.find(username));
+  return true;
 }
 
-User UserManager::queryProfile(const string32& curUser, const string32& username) {
-    if (!isLoggedIn(curUser)) return User();
-    
-    auto users = userDB.find(username);
-    if (users.empty()) return User();
-    
-    if (curUser != username && getPrivilege(curUser) <= users[0].privilege) 
-        return User();
-    
-    return users[0];
+User UserManager::queryProfile(const string32 &curUser,
+                               const string32 &username) {
+  if (!isLoggedIn(curUser))
+    return User();
+
+  auto users = userDB.find(username);
+  if (users.empty())
+    return User();
+
+  if (curUser != username && getPrivilege(curUser) <= users[0].privilege)
+    return User();
+
+  return users[0];
 }
 
-bool UserManager::modifyProfile(const string32& curUser, const string32& username,
-                              const string32& password, const string32& name,
-                              const string32& mailAddr, int privilege) {
-    if (!isLoggedIn(curUser)) return false;
-    
-    auto users = userDB.find(username);
-    if (users.empty()) return false;
-    
-    if (curUser != username && getPrivilege(curUser) <= users[0].privilege)
-        return false;
-    
-    if (privilege != -1 && getPrivilege(curUser) <= privilege)
-        return false;
-    
-    User modified = users[0];
-    if (!password.empty()) modified.password = password;
-    if (!name.empty()) modified.name = name;
-    if (!mailAddr.empty()) modified.mailAddr = mailAddr;
-    if (privilege != -1) modified.privilege = privilege;
-    
-    userDB.remove(username, users[0]);
-    userDB.insert(username, modified);
-    return true;
+User UserManager::modifyProfile(const string32 &curUser,
+                                const string32 &username,
+                                const string32 &password, const string32 &name,
+                                const string32 &mailAddr, int privilege) {
+  if (!isLoggedIn(curUser))
+    return User();
+
+  auto users = userDB.find(username);
+  if (users.empty())
+    return User();
+
+  if (curUser != username && getPrivilege(curUser) <= users[0].privilege)
+    return User();
+
+  if (privilege != -1 && getPrivilege(curUser) <= privilege)
+    return User();
+
+  User modified = users[0];
+  if (!password.empty())
+    modified.password = password;
+  if (!name.empty())
+    modified.name = name;
+  if (!mailAddr.empty())
+    modified.mailAddr = mailAddr;
+  if (privilege != -1)
+    modified.privilege = privilege;
+
+  userDB.remove(username, users[0]);
+  userDB.insert(username, modified);
+  return modified;
 }
 
-bool UserManager::isLoggedIn(const string32& username) const {
-    return loggedInUsers.find(username) != loggedInUsers.cend();
+bool UserManager::isLoggedIn(const string32 &username) const {
+  return loggedInUsers.find(username) != loggedInUsers.cend();
 }
 
-int UserManager::getPrivilege(const string32& username) const {
-    auto it = loggedInUsers.find(username);
-    if (it != loggedInUsers.cend()) return it->second;
-    return -1;
+int UserManager::getPrivilege(const string32 &username) const {
+  auto it = loggedInUsers.find(username);
+  if (it != loggedInUsers.cend())
+    return it->second;
+  return -1;
 }
 
-bool UserManager::isFirstUser() const {
-    return userDB.isEmpty;
-}
+bool UserManager::isFirstUser() const { return userDB.isEmpty; }
 
 #endif // USER_MANAGER_HPP
--- src/submit/TicketSystem.cpp
+++ src/submit/TicketSystem.cpp
@@ -1,123 +1,98 @@
-#include <iostream>
-#include "stl/map.hpp"
-#include "utils/commandParser.hpp"
-#include "services/userManager.hpp"
 #include "services/orderManager.hpp"
 #include "services/trainManager.hpp"
+#include "services/userManager.hpp"
+#include "stl/map.hpp"
+#include "utils/commandParser.hpp"
+#include <iostream>
 
 using sjtu::map;
 
 int main() {
-    UserManager userManager("users");
-    // TrainManager trainManager;
-    // OrderManager orderManager(&userManager, &trainManager);
-    
-    std::string line;
-    while (getline(std::cin, line)) {
-        int timestamp;
-        std::string command;
-        map<char, std::string> params;
-        
-        if (!CommandParser::parse(line, timestamp, command, params)) {
-            std::cout << "[" << timestamp << "] -1" << std::endl;
-            continue;
-        }
-        
-        std::cout << "[" << timestamp << "] ";
-        
-        try {
-            if (command == "add_user") {
-                std::cout << userManager.addUser(
-                    params['c'], params['u'], params['p'],
-                    params['n'], params['m'], std::stoi(params['g'])
-                );
-            }
-            else if (command == "login") {
-                std::cout << userManager.login(params['u'], params['p']);
-            }
-            else if (command == "logout") {
-                std::cout << userManager.logout(params['u']);
-            }
-            else if (command == "query_profile") {
-                std::string result = userManager.queryProfile(params['c'], params['u']);
-                std::cout << (result.empty() ? "-1" : result);
-            }
-            else if (command == "modify_profile") {
-                std::string result = userManager.modifyProfile(
-                    params['c'], params['u'],
-                    params.count('p') ? params['p'] : "",
-                    params.count('n') ? params['n'] : "",
-                    params.count('m') ? params['m'] : "",
-                    params.count('g') ? std::stoi(params['g']) : -1
-                );
-                std::cout << (result.empty() ? "-1" : result);
-            }
-            else if (command == "add_train") {
-                std::cout << trainManager.addTrain(
-                    params['i'], std::stoi(params['n']), std::stoi(params['m']),
-                    params['s'], params['p'], params['x'],
-                    params['t'], params['o'], params['d'], params['y']
-                );
-            }
-            else if (command == "delete_train") {
-                std::cout << trainManager.deleteTrain(params['i']);
-            }
-            else if (command == "release_train") {
-                std::cout << trainManager.releaseTrain(params['i']);
-            }
-            else if (command == "query_train") {
-                std::string result = trainManager.queryTrain(params['i'], params['d']);
-                std::cout << (result.empty() ? "-1" : result);
-            }
-            else if (command == "query_ticket") {
-                std::string sortBy = params.count('p') ? params['p'] : "time";
-                std::string result = trainManager.queryTicket(
-                    params['s'], params['t'], params['d'], sortBy
-                );
-                std::cout << (result.empty() ? "0" : result);
-            }
-            else if (command == "query_transfer") {
-                std::string sortBy = params.count('p') ? params['p'] : "time";
-                std::string result = trainManager.queryTransfer(
-                    params['s'], params['t'], params['d'], sortBy
-                );
-                std::cout << (result.empty() ? "0" : result);
-            }
-            else if (command == "buy_ticket") {
-                bool queue = params.count('q') && params['q'] == "true";
-                std::string result = orderManager.buyTicket(
-                    params['u'], params['i'], params['d'],
-                    std::stoi(params['n']), params['f'], params['t'], queue
-                );
-                std::cout << result;
-            }
-            else if (command == "query_order") {
-                std::string result = orderManager.queryOrder(params['u']);
-                std::cout << (result.empty() ? "-1" : result);
-            }
-            else if (command == "refund_ticket") {
-                int n = params.count('n') ? std::stoi(params['n']) : 1;
-                std::cout << orderManager.refundTicket(params['u'], n);
-            }
-            else if (command == "clean") {
-                userManager.clean();
-                trainManager.clean();
-                orderManager.clean();
-                std::cout << "0";
-            }
-            else if (command == "exit") {
-                userManager.cleanLoggedInUsers();
-                std::cout << "bye";
-                break;
-            }
-            else {
-                std::cout << "-1"; // Unknown command
-            }
-        } catch (const std::exception& e) {
-            std::cout << "-1"; // Handle any parsing errors
-        }
-        
-        std::cout << std::endl;
+  UserManager userManager("users");
+  // TrainManager trainManager;
+  // OrderManager orderManager(&userManager, &trainManager);
+
+  std::string line;
+  while (getline(std::cin, line)) {
+    int timestamp;
+    std::string command;
+    map<char, std::string> params;
+
+    if (!CommandParser::parse(line, timestamp, command, params)) {
+      std::cout << "[" << timestamp << "] -1" << std::endl;
+      continue;
     }
-    return 0;
+
+    std::cout << "[" << timestamp << "] ";
+
+    try {
+      if (command == "add_user") {
+        std::cout << userManager.addUser(params['c'], params['u'], params['p'],
+                                         params['n'], params['m'],
+                                         std::stoi(params['g']));
+      } else if (command == "login") {
+        std::cout << (userManager.login(params['u'], params['p']) ? "0" : "-1");
+      } else if (command == "logout") {
+        std::cout << (userManager.logout(params['u']) ? "0" : "-1");
+      } else if (command == "query_profile") {
+        std::cout << userManager.queryProfile(params['c'], params['u']);
+      } else if (command == "modify_profile") {
+        std::cout << userManager.modifyProfile(
+            params['c'], params['u'], params.count('p') ? params['p'] : "",
+            params.count('n') ? params['n'] : "",
+            params.count('m') ? params['m'] : "",
+            params.count('g') ? std::stoi(params['g']) : -1);
+      } else if (command == "add_train") {
+        std::cout << trainManager.addTrain(
+            params['i'], std::stoi(params['n']), std::stoi(params['m']),
+            params['s'], params['p'], params['x'], params['t'], params['o'],
+            params['d'], params['y']);
+      } else if (command == "delete_train") {
+        std::cout << trainManager.deleteTrain(params['i']);
+      } else if (command == "release_train") {
+        std::cout << trainManager.releaseTrain(params['i']);
+      } else if (command == "query_train") {
+        std::string result = trainManager.queryTrain(params['i'], params['d']);
+        std::cout << (result.empty() ? "-1" : result);
+      } else if (command == "query_ticket") {
+        std::string sortBy = params.count('p') ? params['p'] : "time";
+        std::string result = trainManager.queryTicket(params['s'], params['t'],
+                                                      params['d'], sortBy);
+        std::cout << (result.empty() ? "0" : result);
+      } else if (command == "query_transfer") {
+        std::string sortBy = params.count('p') ? params['p'] : "time";
+        std::string result = trainManager.queryTransfer(
+            params['s'], params['t'], params['d'], sortBy);
+        std::cout << (result.empty() ? "0" : result);
+      } else if (command == "buy_ticket") {
+        bool queue = params.count('q') && params['q'] == "true";
+        std::string result = orderManager.buyTicket(
+            params['u'], params['i'], params['d'], std::stoi(params['n']),
+            params['f'], params['t'], queue);
+        std::cout << result;
+      } else if (command == "query_order") {
+        std::string result = orderManager.queryOrder(params['u']);
+        std::cout << (result.empty() ? "-1" : result);
+      } else if (command == "refund_ticket") {
+        int n = params.count('n') ? std::stoi(params['n']) : 1;
+        std::cout << orderManager.refundTicket(params['u'], n);
+      } else if (command == "clean") {
+        userManager.clean();
+        trainManager.clean();
+        orderManager.clean();
+        std::cout << "0";
+      } else if (command == "exit") {
+        userManager.cleanLoggedInUsers();
+        std::cout << "bye";
+        break;
+      } else {
+        std::cout << "-1"; // Unknown command
+      }
+    } catch (const std::exception &e) {
+      std::cout << "-1"; // Handle any parsing errors
+    }
+
+    std::cout << std::endl;
+  }
+  return 0;
 }
